1c7365e75780af54b18ca5ba5e2f1eb7e6f7e53e
Luis Murias <murias@aenred.com>
2011-03-26 13:40:10 +0100

    Adding config layout and some resources

--- gen/com/aenred/cpbuslocator/R.java
+++ gen/com/aenred/cpbuslocator/R.java
@@ -10,14 +10,31 @@ package com.aenred.cpbuslocator;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int configEditViewHeight=0x7f040000;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int editTextServerName=0x7f060005;
+        public static final int editTextTimeGPS=0x7f060001;
+        public static final int editTextTimeServer=0x7f060007;
+        public static final int textViewServerActivate=0x7f060002;
+        public static final int textViewServerName=0x7f060004;
+        public static final int textViewTimeGPS=0x7f060000;
+        public static final int textViewTimeServer=0x7f060006;
+        public static final int toggleButtonServerActivate=0x7f060003;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int config=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050000;
+        public static final int serverActivate=0x7f050002;
+        public static final int serverName=0x7f050003;
+        public static final int serverTime=0x7f050004;
+        public static final int timeGPS=0x7f050001;
     }
 }
